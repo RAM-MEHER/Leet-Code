e0ea4769ebe2c96665c105ef9cf2eeb8fd321d4f
RAM-MEHER <147472978+RAM-MEHER@users.noreply.github.com>
2025-03-07 23:30:15 +0530

    Time: 82 ms (79.60%) | Memory: 44.7 MB (70.80%) - LeetSync

---
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.java
@@ -0,0 +1,54 @@
+// import org.apache.commons.math3.primes.Primes;
+class Solution 
+{
+    public int[] closestPrimes(int left, int right) 
+    {
+        int []ans = new int[]{-1,-1};
+        int i , first = 0 , second = 0, count = 0 , diff , min_diff = Integer.MAX_VALUE;
+        boolean []primes = new boolean[right+1];
+        Arrays.fill(primes , true);
+
+        primes[0] = primes[1] = false;
+
+        for(i = 2 ; i <= Math.sqrt(right) ; i++)
+        {
+            if(primes[i])
+            {
+                for(int j = i*i ; j <= right ; j+=i)
+                    primes[j] = false;
+            }
+        }
+
+        for(i = left ; i <= right ; i++)
+        {
+            if(primes[i] && i > 1)
+            {
+                count++;
+                if(count % 2 == 1 && count == 1)
+                    first = i;
+                else if(count % 2 == 0 && count == 2)
+                    second = i;
+                else
+                {
+                    first = second;
+                    second = i;
+                }
+// Upto Counting the Primes. -----------------------------------------------------
+                if(count >= 2)
+                {
+                    diff = second - first;
+
+                    if(diff < min_diff)
+                    {
+                        ans[0] = first;
+                        ans[1] = second;
+                        min_diff = diff;
+                    }
+                }
+            }
+
+        }
+
+        return ans;
+    }
+}

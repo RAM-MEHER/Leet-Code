2efb474dbfc08bb254d1527cddb2abecb7db1801
RAM-MEHER <147472978+RAM-MEHER@users.noreply.github.com>
2026-06-08 14:19:59 +0530

    Time: 7 ms (11.70%) | Memory: 47.9 MB (5.28%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,57 @@
+class Solution 
+{
+    public int[] asteroidCollision(int[] asteroids) 
+    {
+        Stack<Integer> st = new Stack<>();
+        
+        for(int i = 0 ; i < asteroids.length ; i++)
+        {
+            if(st.isEmpty())
+                st.push(asteroids[i]);
+            else
+            {
+                if(st.peek() * asteroids[i] > 0)
+                    st.push(asteroids[i]);
+                else
+                {
+                    if((st.peek() > 0) && (asteroids[i] < 0))
+                    {
+                        while(true)
+                        {
+                            if(st.isEmpty())
+                            {
+                                st.push(asteroids[i]);
+                                break;
+                            }
+                            else if((st.peek() > 0) && (st.peek() < Math.abs(asteroids[i])))
+                                st.pop();
+                            else if((st.peek() > 0) && (st.peek() > Math.abs(asteroids[i])))
+                                break;
+                            else if((st.peek() > 0) && (st.peek() == Math.abs(asteroids[i])))
+                            {
+                                st.pop();
+                                break;
+                            }
+                            else
+                            {
+                                st.push(asteroids[i]);
+                                break;
+                            }
+                        }
+                    }
+                    else
+                        st.push(asteroids[i]);
+                }
+            }
+        }
+        int []ans = new int[st.size()];
+        int p = 0;
+        for(int i : st)
+        {
+            ans[p++] = i;
+        }
+        st.clear();
+
+        return ans; 
+    }
+}

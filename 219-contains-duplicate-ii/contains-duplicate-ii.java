3e4641a187ff9a0ce8f8d1bc1d6b1687bc6801d3
RAM-MEHER <147472978+RAM-MEHER@users.noreply.github.com>
2026-06-11 10:41:48 +0530

    Time: 23 ms (80.27%) | Memory: 90.7 MB (34.68%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -2,26 +2,17 @@ class Solution
 {
     public boolean containsNearbyDuplicate(int[] nums, int k) 
     {
-        if(k == 0) return false;
-        HashMap<Integer , Integer> map = new HashMap<>();
-        for(int i = 0 ; i < nums.length ; i++)
-        {
-            if(map.containsKey(nums[i]))
-            {
-                if((i-map.get(nums[i])) <= k)
+        HashMap<Integer , Integer> map = new HashMap<>(); 
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                int b=map.get(nums[i]);
+                if(i-b<=k){
                     return true;
-                else if((i-map.get(nums[i])) > k)
-                    map.put(nums[i] , i);
+                }
+
             }
-            else
-                map.put(nums[i] , i);
+            map.put(nums[i],i);
         }
         return false;
     }
 }
-
-/*
-Time Complexity : O(n)
-Space Complexity : O(n)
-can be optimize using hashset instead of hashmap
-*/

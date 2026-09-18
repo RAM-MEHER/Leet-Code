1cb6cac09130cf30f6554844b424655f35d1e9f6
RAM-MEHER <147472978+RAM-MEHER@users.noreply.github.com>
2026-09-18 11:17:21 +0530

    Time: 1 ms (100.00%) | Memory: 46.2 MB (78.25%) - LeetSync

--- 1184-car-pooling/car-pooling.java
+++ 1184-car-pooling/car-pooling.java
@@ -2,25 +2,19 @@ class Solution
 {
     public boolean carPooling(int[][] trips, int capacity) 
     {
-        Arrays.sort(trips, (a, b) -> 
-        {
-            if (a[1] != b[1]) 
-                return Integer.compare(a[1], b[1]);
-            return Integer.compare(a[2], b[2]);
-        }); 
+        int noOfTrips = trips.length , countOfPass = 0;
+        int passengers[] = new int[1001];
 
-        int len = trips.length , cap = 0;
-
-        for(int i = 0 ; i < len ; ++i)
+        for(int i = 0 ; i < noOfTrips ; ++i)
         {
-            cap = 0;
-            for(int j = i ; j >= 0 ; --j)
-            {
-                if(trips[i][1] < trips[j][2])
-                    cap += trips[j][0];
-                if(cap > capacity)
-                    return false;
-            }
+            passengers[trips[i][1]] += trips[i][0];
+            passengers[trips[i][2]] -= trips[i][0];
+        }
+        for(int noOfPass : passengers)
+        {
+            countOfPass += noOfPass;
+            if(countOfPass > capacity)
+                return false;
         }
         return true;
     }
